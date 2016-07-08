ad056dbd602e329c0eb5c0a134632deaadffeb9f
tarioto <timarioto@Tims-MacBook-Pro.local>
2016-07-08 13:02:31 -0700

    still fixing https errors

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function pokeList1() {
     };
     input.placeholder = "Loading pokemon..."; //while pokemon load in the data list
 
-    request.open("GET", "//pokeapi.co/api/v2/pokemon/?limit=811", true); //use the GET call to go to the list of pokemon from the api
+    request.open("GET", "https//pokeapi.co/api/v2/pokemon/?limit=811", true); //use the GET call to go to the list of pokemon from the api
     request.send(); //send XMLHttpRequest
 }
 
@@ -50,7 +50,7 @@ function pokeList2() { //same as pokeList1 but for the other persons pokemon
     };
     input.placeholder = "Loading pokemon...";
 
-    request.open("GET", "//pokeapi.co/api/v2/pokemon/?limit=811", true);
+    request.open("GET", "https//pokeapi.co/api/v2/pokemon/?limit=811", true);
     request.send();
 }
 
@@ -62,7 +62,7 @@ function pkmn(number) {//display pokemon name type picture create a chart and po
             pokeInfo(request.responseText);
         }
     };
-    request.open("GET", "//pokeapi.co/api/v2/pokemon/" + poke, true);
+    request.open("GET", "https//pokeapi.co/api/v2/pokemon/" + poke, true);
     request.send();
 
     function pokeInfo(response) {//display name type picture from the api request to html
@@ -217,7 +217,7 @@ function strengthsWeaknesses(number) {//create table of pokemons strengths and w
         }
     };
     var type = deCapitalizeFirstLetter(document.getElementById("type" + number).innerHTML.split(" ")[0]);
-    request.open("GET", "//pokeapi.co/api/v2/type/" + type, true);
+    request.open("GET", "https//pokeapi.co/api/v2/type/" + type, true);
     request.send();
 
     function damageTable(response) {//use pokemon type object to create a table of strengths and weaknesses
